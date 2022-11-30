08bbcc4a740c990a791ca9d544762306a20619a8
saitek <saitek300@gmail.com>
2022-11-30 08:05:33 -0800

    addNewNote func. for the backend

---
+++ Develop/Helpers/addNewNote.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+
+const addNewNote = (storedNotes, addNote) => {
+    fs.readFile(storedNotes, 'utf8', (err, data) => {
+        if (err) {
+            console.log('there was an error')
+        } else {
+            console.log('test')
+            const parsedNotes = JSON.parse(data)
+            parsedNotes.push(addNote)
+
+            fs.writeFile(storedNotes, JSON.stringify(parsedNotes, null, 2), (err) => {
+                if (err) {
+                    console.log(err)
+                } else {
+                    console.log('success')
+                }
+            })
+        }
+    })
+}
+
+module.exports = addNewNote;
